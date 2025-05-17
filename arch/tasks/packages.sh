11683f3c5f78f0d16a6bddd6919bbeb68d502411
Yuvraj Birdi <ysbirdi@uwaterloo.ca>
2025-05-17 13:08:34 -0400

    automated dev update: moving on to post install stuff currently: packages.sh

--- arch/tasks/packages.sh
+++ arch/tasks/packages.sh
@@ -30,5 +30,5 @@ run(){
     sudo pacman -Syu --noconfirm --needed dialog xdg-user-dirs xdg-utils xdg-desktop-portal-gtk pipewire-pulse gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups alsa-utils pipewire pipewire-alsa pipewire-pulse bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu-desktop qemu edk2-ovmf bridge-utils dnsmasq vde2 iptables-nft ipset firewalld sof-firmware nss-mdns acpid os-prober ntfs-3g
 
 
-    sudo paru -Syu --noconfirm --needed alacritty btop brave code discord dunst emacs fish ttf-jetbrains-mono-nerd lf light mpv neofetch notion-app-enhanced ookla-speedtest-bin qbittorrent ranger rofi spotify spicetify-cli thunar vlc python-pywal zathura 
+    paru -Syu --noconfirm --needed alacritty btop brave code discord dunst emacs fish ttf-jetbrains-mono-nerd lf light mpv neofetch notion-app-enhanced ookla-speedtest-bin qbittorrent ranger rofi spotify spicetify-cli thunar vlc python-pywal zathura zathura-pdf-mupdf zathura-djvu zathura-pywal zathura-epub-mupdf
 }
